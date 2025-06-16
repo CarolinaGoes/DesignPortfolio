8ff550c57a36fe1e6a4b8e1df78b0852a23caf25
CarolinaGoes <carolinarocha.89@hotmail.com>
2025-06-16 13:54:08 -0300

    .

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,9 +1,8 @@
 import type { Config } from "tailwindcss";
-import './index.css';
 
 export default {
   darkMode: ["class"],
-  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       borderRadius: {
