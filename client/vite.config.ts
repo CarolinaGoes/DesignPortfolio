edccd5750cf8319a87ac277674d6349c1a2e2ee7
CarolinaGoes <carolinarocha.89@hotmail.com>
2025-06-19 13:00:10 -0300

    .

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths({
-      root: '../', // Aponte para a raiz onde está o tsconfig.json
-      projects: ['../tsconfig.json'] // Caminho para o tsconfig principal
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true // Para debug
   }
 })
