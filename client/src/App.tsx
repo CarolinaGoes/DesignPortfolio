7a244b3a6a9e0536c0600079a9d66b6b0583bbe7
CarolinaGoes <carolinarocha.89@hotmail.com>
2025-07-06 14:23:54 -0300

    Add wouter router and update about section content
    Added 'wouter' as a dependency and refactored routing in App.tsx to use the latest wouter API. Updated the 'about' section in both English and Portuguese locale files with new personal information, education, and experience, replacing the previous structure.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,27 @@
-import { Switch, Route } from "wouter";
+import { Route } from "wouter";
 import { queryClient } from "./lib/queryClient.js";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster";
 import NotFound from "./pages/not-found";
 import Home from "./pages/home";
 
-
-function Router() {
+function AppRouter() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
+    </>
   );
 }
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <AppRouter />
       <Toaster />
     </QueryClientProvider>
   );
